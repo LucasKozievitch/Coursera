26b69eb3710bf1be8b9bd95f247061de5fff9161
Lucas Amorim Kozievitch <100216396+LucasKozievitch@users.noreply.github.com>
2022-03-09 16:03:01 -0300

    Delete Exercício 1 ad.py

--- Exercício 1 ad.py
+++
@@ -1,9 +0,0 @@
-#Entrada de dados
-nome_cliente = input('Digite o nome do cliente: ')
-dia_vencimento = input('Digite o dia de vencimento: ')
-mes_vencimento = input('Digite o mês de vencimento: ')
-valor_fatura = input('Digite o valor da fatura: ')
-
-#Saída de dados
-print('Olá, {} '.format(nome_cliente),'\nA sua fatura com vencimento em {}'.format(dia_vencimento),
-      'de {}'.format(mes_vencimento),'no valor de R$ {}'.format(valor_fatura),'está fechada.')
